f4e9e8499fd524119671032ae5e4b565513c90c7
Tony Peng <283458694@qq.com>
2019-12-11 15:46:12 +0800

    add css

--- eclaim-app/src/app/app.module.ts
+++ eclaim-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatConversationComponent } from './chat-conversation/chat-conversation.component';
+import { ChatFooterComponent } from './chat-footer/chat-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatConversationComponent,
+    ChatFooterComponent
   ],
   imports: [
     BrowserModule,
